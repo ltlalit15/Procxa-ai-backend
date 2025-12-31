45e842add7fbb03651c00a9a67f88e065be3b76a
GautamBairagi <gauatambairagi22@gmail.com>
2025-12-31 12:14:55 +0530

    sadad

--- procurement_server.js
+++ procurement_server.js
@@ -18,11 +18,35 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static(path.join(__dirname, "public" )));
 app.use(cookieParser());
 
+// app.use(cors({
+//     credentials: true,
+//     origin : "*",
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     allowedHeaders:[ 'Content-Type','Authorization', 'X-Refresh-Token']
+// }));
+
+
+
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://procxa.netlify.app"
+];
+
 app.use(cors({
-    credentials: true,
-    origin : "*",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders:[ 'Content-Type','Authorization', 'X-Refresh-Token']
+  credentials: true,
+  origin: function (origin, callback) {
+    // Allow requests with no origin (Postman, curl, mobile apps)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("CORS not allowed from this origin"));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Refresh-Token']
 }));
 
 const mainRoutes = require("./src/routes/main.routes")
